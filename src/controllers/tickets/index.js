5b009d31df3af9d263c6f6f0f8e7ff6226d04503
Thiago <thiagonfk@gmail.com>
2026-09-07 19:02:46 -0300

    feat: add parâmetros dinâmicos

--- src/controllers/tickets/index.js
+++ src/controllers/tickets/index.js
@@ -1,4 +1,6 @@
 export function index({ request, response, database }){
+    const { status } = request.query
+    console.log(status)
     const tickets = database.select("tickets")
     
     return response.end(JSON.stringify(tickets))
--- src/middlewares/routeHandler.js
+++ src/middlewares/routeHandler.js
@@ -1,14 +1,20 @@
 import { routes } from "../routes/index.js";
 import { Database } from "../database/database.js";
+import { extractQueryParams } from "../utils/extractQueryParams.js";
 
 const database = new Database();
 
 export function routeHandler(request, response) {
   const route = routes.find((route) => {
-    return route.method === request.method && route.path === request.url;
+    return route.method === request.method && route.path.test(request.url);
   });
 
   if (route) {
+    const routeParams = request.url.match(route.path)
+
+    const { query } = routeParams.groups
+    request.query = query ? extractQueryParams(query) : {};
+
     return route.controller({ request, response, database });
   }
 
