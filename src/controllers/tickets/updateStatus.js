1192da30b5a470ee71f485b8a2934351b4a80a2a
Thiago <thiagonfk@gmail.com>
2026-09-07 22:49:34 -0300

    feat: add método de update status

---
+++ src/controllers/tickets/updateStatus.js
@@ -0,0 +1,5 @@
+export function updateStatus({ request, response, database}) {
+    const { id } = request.params;
+    database.update("tickets", id, {status: "closed"})
+    return response.end()
+}
