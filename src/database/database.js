e9fffbb9e3581bce676f2f37d0e3448b672082be
Thiago <thiagonfk@gmail.com>
2026-09-07 23:11:47 -0300

    feat: add método delete

--- src/database/database.js
+++ src/database/database.js
@@ -46,13 +46,22 @@ export class Database {
   update(table, id, data) {
     const rowIndex = this.#database[table].findIndex((row) => row.id === id);
 
-    if(rowIndex > -1) {
+    if (rowIndex > -1) {
       this.#database[table][rowIndex] = {
         ...this.#database[table][rowIndex],
-        ...data
-      }
+        ...data,
+      };
     }
 
-    this.#persist()
+    this.#persist();
+  }
+
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id);
+
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1);
+      this.#persist();
+    }
   }
 }
